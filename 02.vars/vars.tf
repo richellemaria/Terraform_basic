930dd2eb87cd1e49471606872ca761e4d70eedcc
richellemaria <rishdsouza@gmail.com>
2024-02-14 22:42:47 +0530

    auto push

--- 02.vars/vars.tf
+++ 02.vars/vars.tf
@@ -10,3 +10,16 @@ output "sample-op-var" {
 
     value = "the value of variable is ${var.sample}"
 }
+
+variable "example-list" {
+    default = [
+        "Richelle"
+        "maria"
+        "Dsouza"
+        26
+    ]
+}
+
+output "example_list_op" {
+    value = "My name is ${var.example_list[0]} ${var.example_list[1]} ${var.example_list[2]} born on ${var.example_list[3]}"
+}
