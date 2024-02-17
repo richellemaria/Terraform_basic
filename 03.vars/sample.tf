58c65178e6492c1ae26966c94c7191dc50e4ee97
richellemaria <rishdsouza@gmail.com>
2024-02-17 12:43:36 +0530

    auto push

---
+++ 03.vars/sample.tf
@@ -0,0 +1,13 @@
+variable "city"{}
+
+output "city_name"{
+
+    value = "Our city name is ${var.city}"
+}
+
+variable "state"{}
+
+output "state_name"{
+
+    value = "Our city name is ${var.state}"
+}
