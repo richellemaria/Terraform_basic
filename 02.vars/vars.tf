2f97cb4fa93706c60a8e4b0a5ce477a806bf1628
richellemaria <rishdsouza@gmail.com>
2024-02-17 11:18:29 +0530

    auto push

--- 02.vars/vars.tf
+++ 02.vars/vars.tf
@@ -23,3 +23,17 @@ variable "example_list" {
 output "example_list_op" {
     value = "My name is ${var.example_list[0]} ${var.example_list[1]} ${var.example_list[2]} born on ${var.example_list[3]}"
 }
+
+variable "example_map" {
+
+    default ={
+        Training = "Devops"
+        Topic = "Terraform"
+        day = "Tuesday"
+    }
+}
+
+output "example_map_op" {
+
+    value = "I am Training in ${var.example_map[Training]} and topic is ${var.example_map[Topic]} and today is ${var.example_map[day]}"
+}
