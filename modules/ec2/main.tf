2492a5d86af4b3412e096b72aed8812b36e449ca
richellemaria <rishdsouza@gmail.com>
2024-02-24 00:00:36 +0530

    auto push

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,6 +2,21 @@ resource "aws_instance" "server" {
   ami                     = data.aws_ami.image.id
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [var.sg]
+
+  provisioner "remote-exec" {
+
+    # connection block establishes connection to this
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip             # aws_instance.sample.private_ip : Use this only if your provisioner is outside the resource.
+    }
+
+    inline = [
+      "ansible-pull -U https://github.com/b54-clouddevops/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml"
+    ]
+  }
 }
 
 variable "sg"{}
