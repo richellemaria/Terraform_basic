7aa7f197252a755e1cfd4d35a01bb1454a851afc
richellemaria <rishdsouza@gmail.com>
2024-02-23 20:14:29 +0530

    auto push

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "server" {
-  ami                     = "data.aws_ami.image.id"
+  ami                     = data.aws_ami.image.id
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [var.sg]
 }
